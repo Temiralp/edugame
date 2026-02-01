4f04677357ad90e2d50ca11e9c5a6174388bf486
isokurt <ismailkicri34@gmail.com>
2026-02-01 16:01:35 +0300

    ismail playercontroller degistirildi

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,13 +8,9 @@ public class PlayerController
 
 public class PlayerCameraController
 {
+    int i;
 
 
-<<<<<<< HEAD
-    test
-}
-=======
     
 }
 
->>>>>>> b283342c2f4db658cf34e1d6ad28ea2e305269da
