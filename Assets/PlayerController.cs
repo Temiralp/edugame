d03ec907a94a6d565dd534f869e12c42b1fdba23
Ibrahim şafak <ibrahim.ktk01@gmail.com>
2026-02-01 16:36:30 +0300

    test1234 eklendi player düzeltildi

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,18 +3,7 @@ using UnityEngine;
 
 public class PlayerController
 {
-    test1
+    test1234
 }
 
-<<<<<<< HEAD
-public class PlayerCameraController
-{
-    int i;
 
-
-    
-}
-
-=======
-
->>>>>>> 846bf3bc0aa79298f6bb2ea7aa8e914ed05998ac
